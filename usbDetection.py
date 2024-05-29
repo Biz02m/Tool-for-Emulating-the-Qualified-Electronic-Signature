0e0e64b32b0e463c37f9d783c48dc6640ce60952
Mikołaj Bisewski <mikolajbisewski@wp.pl>
2024-05-29 16:34:14 +0200

    fixed decrypting the key for testing purpouses

--- usbDetection.py
+++ usbDetection.py
@@ -1,8 +1,11 @@
 import psutil
 import os
+from Crypto.Cipher import AES
+
 
 def get_current_drives():
     return [disk.device for disk in psutil.disk_partitions()]
+
 
 def detect_new_drive():
     print("insert drive:")
@@ -15,50 +18,45 @@ def detect_new_drive():
             print(f'New drive detected: {new_drives[0]}')
             return new_drives[0]
 
-
-def list_files(drive):
-    print(f"Listing files on {drive}:")
+# enables to check whether the file is on the root folder
+# returns the key and the nonce value which is required for deciphering
+# the driver should not be plugged in beforehand
+def get_key_nonce(drive, filename="privateKey.txt"):
     try:
-        for root, dirs, files in os.walk(drive):
-            for name in dirs:
-                print(os.path.join(root, name))
-
-            for name in files:
-                print(os.path.join(root, name))
+        with open(drive + filename, 'rb') as keyFile:
+            aes_nonce = keyFile.read(16)
+            key = keyFile.read()
+            return key, aes_nonce
     except Exception as e:
-        print(f"Could not list files on {drive}: {e}")
-
-#straightforward
-#enables to check whether the file is on the root folder
-#returns the key
-def get_key(drive):
-    try:
-        with open(drive + "privateKey.txt", 'r') as keyFile:
-            key = []
-            for line in keyFile:
-                key.append(line.strip('\n'))
-            return key
-    except Exception as e:
-        print(f"Could not find private key on {drive}: {e}")
+        print(f"Error: {drive}: {e}")
     return []
 
-#enables to check whether the file is
-#in any of the folders on the drive
-def find_private_key_file(drive, filename="privateKey.txt"):
-    print(f"Searching for {filename} on {drive}:")
-    try:
-        for root, dirs, files in os.walk(drive):
-            if filename in files:
-                print(f"File {filename} found at: {os.path.join(root, filename)}")
-                return True
-        print(f"File {filename} is not on the drive")
-    except Exception as e:
-        print(f"Could not search files on {drive}: {e}")
-    return False
+
+# enables to check whether the file is
+# in any of the folders on the drive
+# def find_private_key_file(drive, filename="privateKey.txt"):
+#     print(f"Searching for {filename} on {drive}:")
+#     try:
+#         for root, dirs, files in os.walk(drive):
+#             if filename in files:
+#                 print(f"File {filename} found at: {os.path.join(root, filename)}")
+#                 return True
+#         print(f"File {filename} is not on the drive")
+#     except Exception as e:
+#         print(f"Could not search files on {drive}: {e}")
+#     return False
+
+
+#This function is merely for testing purpouses
+#Deciphering will occur in another script
+def decipher(private_key, aes_nonce):
+    pin = "1234"
+    pin_key = pin.encode() * 16  # Pad PIN is 16 bytes (AES block size)
+    aes_cipher = AES.new(pin_key[:16], AES.MODE_EAX, nonce=aes_nonce)
+    decrypted_private_key = aes_cipher.decrypt(private_key)
+    print(decrypted_private_key.decode())
 
 
 drive = detect_new_drive()
-key = get_key(drive)
-print(key)
-
-
+key, aes_nonce = get_key_nonce(drive)
+decipher(key, aes_nonce)
